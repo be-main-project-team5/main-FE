254d61baaf0440e4428a0c8aa3cadb997c012865
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-06 13:52:06 +0900

    ♻️ Refactor: Input 컴포넌트 TS 마이그레이션 및 타입 정의 (#11)

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,11 +1,28 @@
-function Input() {
-  return (
-    <input
-      type="text"
-      placeholder="placeholder"
-      className="border border-solid border-gray-200"
-    />
-  );
+import { cva, type VariantProps } from 'class-variance-authority';
+import clsx from 'clsx';
+
+const inputStyles = cva(
+  'w-full rounded-[10px] border border-solid border-gray-200 bg-white text-sm font-normal placeholder:text-gray-500 focus:outline-none',
+  {
+    variants: {
+      intent: {
+        default: 'p-3',
+        hasIcon: 'pt-3 pr-8 pb-3 pl-3',
+      },
+    },
+    defaultVariants: {
+      intent: 'default',
+    },
+  },
+);
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
+  VariantProps<typeof inputStyles>;
+
+function Input({ type = 'text', placeholder, intent, className }: InputProps) {
+  const classes = clsx(inputStyles({ intent }), className);
+
+  return <input type={type} placeholder={placeholder} className={classes} />;
 }
 
 export default Input;
