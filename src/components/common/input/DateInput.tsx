1760d98c70bd86bf237e70473020c8b600e1139e
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-06 19:10:57 +0900

    ✨ Feat: DateInput 컴포넌트 추가 (#11)
    DefaultInput 기반으로 날짜 선택 기능 구현 / 클릭 어웨이(click outside) 감지 로직으로 드롭다운 제어 / react-day-picker 사용해 날짜 선택 UI 구성 / Chevron 아이콘 커스터마이징 및 Tailwind 스타일 적용 / disabled 옵션으로 과거 날짜 선택 방지

---
+++ src/components/common/input/DateInput.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from 'react';
+import DefaultInput from './DefaultInput';
+import {
+  CalendarDaysIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from '@heroicons/react/24/outline';
+import {
+  type ChevronProps,
+  DayPicker,
+  getDefaultClassNames,
+} from 'react-day-picker';
+import 'react-day-picker/style.css';
+
+const iconStyles = 'text-gray-500 absolute bottom-2.75 z-10 right-3 h-6 w-6';
+
+const CustomChevron = ({ orientation, ...rest }: ChevronProps) => {
+  return orientation === 'left' ? (
+    <ChevronLeftIcon {...rest} className="h-5 w-5 text-fuchsia-500" />
+  ) : (
+    <ChevronRightIcon {...rest} className="h-5 w-5 text-fuchsia-500" />
+  );
+};
+
+function DateInput() {
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+  const [isSelected, setIsSelected] = useState<Date>();
+  const defaultClassNames = getDefaultClassNames();
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        ref.current &&
+        e.target instanceof Node &&
+        !ref.current.contains(e.target)
+      ) {
+        setIsFocus(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  return (
+    <div ref={ref} className="relative">
+      <DefaultInput
+        onClick={() => setIsFocus(true)}
+        value={isSelected?.toLocaleDateString()}
+        readOnly
+        type="text"
+        label="날짜"
+      />
+      <CalendarDaysIcon className={iconStyles} />
+      {isFocus && (
+        <div className="flex w-full justify-center">
+          <div className="absolute w-fit">
+            <DayPicker
+              animate
+              mode="single"
+              selected={isSelected}
+              onSelect={setIsSelected}
+              disabled={{ before: new Date() }}
+              classNames={{
+                today: `text-fuchsia-500`,
+                selected: `bg-fuchsia-400 text-white rounded-full`,
+                root: `${defaultClassNames.root} shadow-lg p-5  bg-white z-100`,
+              }}
+              components={{
+                Chevron: CustomChevron,
+              }}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DateInput;
