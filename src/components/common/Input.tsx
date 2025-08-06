ccc4cc33efaa100d4f96c3eda953cd1393929150
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-06 11:55:21 +0900

    ✨ Feat: Input.tsx 생성 (#11)
    컴포넌트 기본 구조 추가

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,11 @@
+function Input() {
+  return (
+    <input
+      type="text"
+      placeholder="placeholder"
+      className="border border-solid border-gray-200"
+    />
+  );
+}
+
+export default Input;
