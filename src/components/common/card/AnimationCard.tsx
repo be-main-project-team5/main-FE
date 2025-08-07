a312449e6f04eb72beb3c078c74ce4eaf50ad755
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-08 02:01:48 +0900

    ♻️ Refactor: AnimationCard 이미지 고정 (#17)

--- src/components/common/card/AnimationCard.tsx
+++ src/components/common/card/AnimationCard.tsx
@@ -10,19 +10,17 @@ interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
 const cardStyles = `relative bottom-0 flex w-72 scale-100 flex-col items-center gap-5 transition-all duration-300 ease-out hover:scale-110 sm:hover:bottom-8 sm:hover:scale-100`;
 
 function AnimationCard({
-  imageSrc = '',
   title = '',
   description = '',
   className,
   ...rest
 }: CardProps) {
   const classes = clsx(cardStyles, className);
-  const src = imageSrc || placeholderSmall;
 
   return (
     <div className={classes} {...rest}>
       <img
-        src={src}
+        src={placeholderSmall}
         alt={title}
         loading="lazy"
         className="rounded-xl shadow-lg"
