b4320992db236f67588e2d90acf4b5746e941c4f
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-08 02:09:09 +0900

    ✨ Feat: IdolCard 컴포넌트 추가 (#17)
    HeartIcon과 state를 사용해 좋아요 토글 기능 구현/ 이미지 src 없을 때 UserIcon으로 대체 UI 제공 / hover 상태에서 그라데이션 오버레이 및 이름·그룹·포지션 정보 표시

---
+++ src/components/common/card/IdolCard.tsx
@@ -0,0 +1,70 @@
+import { HeartIcon, UserIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import { useState } from 'react';
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  imageSrc?: string;
+  title: string;
+  details: { idolGroup: string; position: string };
+}
+
+function IdolCard({
+  imageSrc = '',
+  title = '',
+  details,
+  className,
+  ...rest
+}: CardProps) {
+  const { idolGroup, position } = details;
+  const [isLiked, setIsLiked] = useState<boolean>();
+
+  const handleClickLike = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setIsLiked(prev => !prev);
+  };
+
+  return (
+    <div
+      className={clsx(
+        'group relative h-89 w-72 cursor-pointer rounded-lg shadow-2xl',
+        className,
+      )}
+      {...rest}
+    >
+      {imageSrc ? (
+        <img
+          src={imageSrc}
+          alt={title}
+          className="h-full w-full rounded-lg object-cover"
+        />
+      ) : (
+        <div className="flex h-full w-full items-center justify-center rounded-lg border-3 border-solid border-neutral-500 bg-neutral-200">
+          <UserIcon className="h-20 w-20 fill-neutral-100 text-neutral-500" />
+        </div>
+      )}
+
+      <div className="transition-filter absolute inset-0 flex h-full w-full items-end rounded-lg bg-gradient-to-b from-transparent to-neutral-800 opacity-0 duration-300 ease-out group-hover:opacity-100">
+        <div onClick={handleClickLike}>
+          {isLiked ? (
+            <HeartIcon className="absolute top-2.75 right-3 z-10 h-12 w-12 fill-fuchsia-500 text-fuchsia-800" />
+          ) : (
+            <HeartIcon className="absolute top-2.75 right-3 z-10 h-12 w-12 fill-neutral-100 text-neutral-500" />
+          )}
+        </div>
+        <div className="flex w-full flex-col gap-2 py-4 text-center opacity-0 transition-all duration-300 ease-out group-hover:opacity-100">
+          <h4 className="text-lg font-semibold text-gray-100">{title}</h4>
+          <div className="flex flex-col gap-1">
+            <span className="text-base font-normal text-gray-200">
+              그룹 : {idolGroup}
+            </span>
+            <span className="text-base font-normal text-gray-200">
+              포지션 : {position}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IdolCard;
