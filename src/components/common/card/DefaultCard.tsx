07b4c3d713ffd6a583161c15a268ef480679dcc6
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-07 17:21:38 +0900

    ♻️ Refactor: DefaultCard 컴포넌트 업데이트 (#17)
    clsx로 스타일 지정 / 타입 개선 / props 추가

--- src/components/common/card/DefaultCard.tsx
+++ src/components/common/card/DefaultCard.tsx
@@ -1,16 +1,26 @@
 import placeholderSmall from '@/assets/images/placeholder-sm.jpg';
+import clsx from 'clsx';
 
-interface CardProps {
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   imageSrc?: string;
   title: string;
   description: string;
 }
 
-function Card({ imageSrc = '', title = '', description = '' }: CardProps) {
+const cardStyles = `relative bottom-0 flex w-72 scale-100 flex-col items-center gap-5 transition-all duration-300 ease-out hover:scale-110 sm:hover:bottom-8 sm:hover:scale-100`;
+
+function Card({
+  imageSrc = '',
+  title = '',
+  description = '',
+  className,
+  ...rest
+}: CardProps) {
+  const classes = clsx(cardStyles, className);
   const src = imageSrc || placeholderSmall;
 
   return (
-    <div className="flex w-72 flex-col items-center gap-5">
+    <div className={classes} {...rest}>
       <img
         src={src}
         alt={title}
