02fab172cbdc8aef635d37f1a42c9f10384c65f3
minji_105 <ajhjmj9@naver.com>
2025-08-07 14:46:17 +0900

    ✨ Feat: select 컴포넌트에 size prop 추가 (#15)
    - 'primary'와 'small' 두 가지 크기를 지원
    - `className` prop을 통해 외부에서 커스텀 클래스를 주입할 수 있도록
      개선

--- src/components/common/Select.tsx
+++ src/components/common/Select.tsx
@@ -4,11 +4,15 @@ import clsx from 'clsx';
 interface SelectProps {
   list: string[];
   placeholder?: string;
+  size?: 'primary' | 'small';
+  className?: string;
 }
 
 export default function Select({
   list,
   placeholder = '옵션을 선택하세요',
+  size = 'primary',
+  className,
 }: SelectProps) {
   const [currentValue, setCurrentValue] = useState<string>(placeholder);
   const [showOptions, setShowOptions] = useState<boolean>(false);
@@ -40,19 +44,32 @@ export default function Select({
 
   return (
     <div
-      className="relative rounded-lg border-1 border-gray-200 bg-white px-3 py-3 text-sm text-gray-500 before:absolute before:top-1 before:right-3 before:text-lg before:content-['⌵']"
+      className={clsx(
+        "relative rounded-lg border-1 border-gray-200 bg-white text-sm text-gray-500 before:absolute before:right-3 before:text-lg before:content-['⌵']",
+        size === 'primary' && 'px-3 py-3 before:top-1',
+        size === 'small' && 'px-2 py-2 before:top-0',
+        className,
+      )}
       onClick={() => setShowOptions(prev => !prev)}
       ref={selectRef}
     >
       <label className="">{currentValue}</label>
       {showOptions && (
-        <ul className="absolute top-12 left-0 w-full rounded-lg border-1 border-gray-200 bg-white p-1">
+        <ul
+          className={clsx(
+            'absolute left-0 z-100 w-full rounded-lg border-1 border-gray-200 bg-white p-1',
+            size === 'primary' && 'top-12',
+            size === 'small' && 'top-10',
+          )}
+        >
           {list.map((data, index) => (
             <li
               key={index}
               className={clsx(
-                'px-8 py-3 text-black hover:bg-gray-50',
+                'text-black hover:bg-gray-50',
                 data === currentValue && 'font-semibold',
+                size === 'primary' && 'px-8 py-3',
+                size === 'small' && 'px-2 py-2',
                 index === 0 && 'rounded-t-md',
                 index === list.length - 1 && 'rounded-b-md',
               )}
