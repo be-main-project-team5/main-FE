c8022a2c1467408a96c8c7d161ddaf8e8a2192dc
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-08 10:40:49 +0900

    ♻️ Refactor: IdolCard 업데이트 (#17)
    imageSrc 없을 때 border 삭제 / 하트 아이콘 스타일 겹치는 부분 clsx로 처리

--- src/components/common/card/IdolCard.tsx
+++ src/components/common/card/IdolCard.tsx
@@ -38,18 +38,21 @@ function IdolCard({
           className="h-full w-full rounded-lg object-cover"
         />
       ) : (
-        <div className="flex h-full w-full items-center justify-center rounded-lg border-3 border-solid border-neutral-500 bg-neutral-200">
+        <div className="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200">
           <UserIcon className="h-20 w-20 fill-neutral-100 text-neutral-500" />
         </div>
       )}
 
       <div className="transition-filter absolute inset-0 flex h-full w-full items-end rounded-lg bg-gradient-to-b from-transparent to-neutral-800 opacity-0 duration-300 ease-out group-hover:opacity-100">
         <div onClick={handleClickLike}>
-          {isLiked ? (
-            <HeartIcon className="absolute top-2.75 right-3 z-10 h-12 w-12 fill-fuchsia-500 text-fuchsia-800" />
-          ) : (
-            <HeartIcon className="absolute top-2.75 right-3 z-10 h-12 w-12 fill-neutral-100 text-neutral-500" />
-          )}
+          <HeartIcon
+            className={clsx(
+              'absolute top-2.75 right-3 z-10 h-12 w-12',
+              isLiked
+                ? 'fill-fuchsia-500 text-fuchsia-800'
+                : 'fill-neutral-100 text-neutral-500',
+            )}
+          />
         </div>
         <div className="flex w-full flex-col gap-2 py-4 text-center opacity-0 transition-all duration-300 ease-out group-hover:opacity-100">
           <h4 className="text-lg font-semibold text-gray-100">{title}</h4>
