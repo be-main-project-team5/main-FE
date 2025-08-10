1ab6291f376441c1fe8cd102c8caecbe5fa857c0
minji_105 <ajhjmj9@naver.com>
2025-08-11 01:31:24 +0900

    🔧 Chore: react/require-default-props 룰 비활성화 (#29)
    PropTypes 기본값 선언이 아닌 TypeScript 기본값을 사용하기 때문에 불필요한 에러 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
     'react/prop-types': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/function-component-definition': [
