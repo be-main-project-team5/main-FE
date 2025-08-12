c971be6803a201423d00d04a9b0d4404e8e31432
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-12 14:36:03 +0900

    ♻️ Refactor: 캘린더 커스텀 툴바 적용 및 개선 (#32)
    월·연도·요일 한국어 표기 / 불필요한 useMemo 제거 / BigCalendar props 추가 및 정리 / 스타일 추가

--- src/components/common/calendar/Calendar.tsx
+++ src/components/common/calendar/Calendar.tsx
@@ -1,16 +1,20 @@
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'dayjs/locale/ko';
 
 import clsx from 'clsx';
 import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
-import { useMemo } from 'react';
+import { useState } from 'react';
 import { Calendar as BigCalendar, dayjsLocalizer } from 'react-big-calendar';
+
+import CalendarToolbar from './CalendarToolbar';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 dayjs.tz.setDefault('Asia/Seoul');
+dayjs.locale('ko');
 
 interface CalendarProps
   extends Partial<React.ComponentProps<typeof BigCalendar>> {
@@ -20,21 +24,32 @@ interface CalendarProps
 const djLocalizer = dayjsLocalizer(dayjs);
 
 function Calendar({ className, ...rest }: CalendarProps) {
-  const { defaultDate, views } = useMemo(
-    () => ({
-      defaultDate: new Date(),
-      views: { month: true },
-    }),
-    [],
-  );
+  const defaultDate = new Date();
+  const [date, setDate] = useState(defaultDate);
+
+  const handleClickDate = (slotInfo: { start: Date }) => {
+    const formattedDate = dayjs(slotInfo.start).format('YYYY-MM-DD');
+    console.log('clicked : ', formattedDate);
+  };
 
   return (
-    <div className={clsx('aspect-square h-[75vh]', className)}>
+    <div
+      className={clsx(
+        'aspect-square h-[75vh] rounded-2xl p-5 shadow-[0_0_20px_5px_#00000015]',
+        className,
+      )}
+    >
       <BigCalendar
+        date={date}
+        onNavigate={newDate => setDate(newDate)}
+        components={{ toolbar: CalendarToolbar }}
         defaultDate={defaultDate}
         localizer={djLocalizer}
-        views={views}
+        views={{ month: true }}
         defaultView="month"
+        formats={{ monthHeaderFormat: 'YYYY년 M월' }}
+        selectable="ignoreEvents"
+        onSelectSlot={handleClickDate}
         {...rest}
       />
     </div>
