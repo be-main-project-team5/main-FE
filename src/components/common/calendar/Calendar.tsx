bb4fe174f2108bd27df5ce41ff4abc1118716b3c
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-12 18:08:25 +0900

    ♻️ Refactor: Calendar 컴포넌트 커스텀 스타일 적용 (#32)
    BigCalendar의 props 적용 및 함수 활용하여 오늘 날짜와 선택된 날짜 커스텀 스타일 적용

--- src/components/common/calendar/Calendar.tsx
+++ src/components/common/calendar/Calendar.tsx
@@ -1,12 +1,17 @@
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'dayjs/locale/ko';
+import './calendar.tw.css';
 
 import clsx from 'clsx';
 import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
-import { useState } from 'react';
-import { Calendar as BigCalendar, dayjsLocalizer } from 'react-big-calendar';
+import { useEffect, useState } from 'react';
+import {
+  Calendar as BigCalendar,
+  dayjsLocalizer,
+  type SlotInfo,
+} from 'react-big-calendar';
 
 import CalendarToolbar from './CalendarToolbar';
 
@@ -24,32 +29,46 @@ interface CalendarProps
 const djLocalizer = dayjsLocalizer(dayjs);
 
 function Calendar({ className, ...rest }: CalendarProps) {
-  const defaultDate = new Date();
-  const [date, setDate] = useState(defaultDate);
+  const todayDate = new Date();
+  const [selectedDate, setSelectedDate] = useState(todayDate);
 
-  const handleClickDate = (slotInfo: { start: Date }) => {
-    const formattedDate = dayjs(slotInfo.start).format('YYYY-MM-DD');
-    console.log('clicked : ', formattedDate);
+  const handleClickDate = (slotInfo: SlotInfo) => {
+    const { start } = slotInfo;
+    // const formattedDate = dayjs(start).format('YYYY-MM-DD');
+    // console.log('clicked : ', formattedDate);
+    setSelectedDate(start);
   };
+
+  const dayPropGetter = (date: Date) => {
+    const isSelected = dayjs(date).isSame(selectedDate, 'day');
+    return { className: isSelected ? 'is-selected' : undefined };
+  };
+
+  useEffect(() => {
+    // console.log(selectedDate);
+  }, [selectedDate]);
 
   return (
     <div
+      data-calendar
       className={clsx(
         'aspect-square h-[75vh] rounded-2xl p-5 shadow-[0_0_20px_5px_#00000015]',
         className,
       )}
     >
       <BigCalendar
-        date={date}
-        onNavigate={newDate => setDate(newDate)}
+        date={selectedDate}
+        onNavigate={newDate => setSelectedDate(newDate)}
         components={{ toolbar: CalendarToolbar }}
-        defaultDate={defaultDate}
+        defaultDate={todayDate}
         localizer={djLocalizer}
         views={{ month: true }}
         defaultView="month"
         formats={{ monthHeaderFormat: 'YYYY년 M월' }}
         selectable="ignoreEvents"
         onSelectSlot={handleClickDate}
+        drilldownView={null}
+        dayPropGetter={dayPropGetter}
         {...rest}
       />
     </div>
