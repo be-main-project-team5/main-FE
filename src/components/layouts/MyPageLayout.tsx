967b7f9ea787ba91238a672be84d2766be2924a2
minji_105 <ajhjmj9@naver.com>
2025-08-13 17:41:09 +0900

    📱 Design: MyPageLayout 반응형 레이아웃 구현 (#40)

--- src/components/layouts/MyPageLayout.tsx
+++ src/components/layouts/MyPageLayout.tsx
@@ -9,18 +9,22 @@ export default function MyPageLayout() {
   return (
     <>
       <Header isLoggedIn={false} />
-      <div className="m-auto mt-16 flex max-w-[1360px] items-stretch px-6 md:px-10">
-        <aside className="flex w-55 flex-col justify-between pr-4">
+      <div className="m-auto mt-16 flex max-w-[1360px] flex-col items-center px-6 md:flex-row md:items-stretch md:px-10">
+        <aside className="hidden w-55 flex-col justify-between pr-4 md:flex">
           <div>
             <h2 className="py-10 text-3xl font-semibold">마이페이지</h2>
             <Nav />
           </div>
-          <BottomNav />
+          <BottomNav className="hidden md:flex" />
         </aside>
+
+        <Nav className="md:hidden" />
 
         <main className="flex-1">
           <Outlet />
         </main>
+
+        <BottomNav className="flex md:hidden" />
       </div>
       <Footer />
     </>
