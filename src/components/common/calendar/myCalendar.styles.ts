3f8423f9ceffbdfef3cf75f81ba0ed1597646e77
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-13 18:06:13 +0900

    ♻️ Refactor: 애니메이션 스타일 수정 (#32)
    날짜 선택이 아닌 날짜 hover 상태일 때 실행되도록 수정

--- src/components/common/calendar/myCalendar.styles.ts
+++ src/components/common/calendar/myCalendar.styles.ts
@@ -1,24 +1,19 @@
 import clsx from 'clsx';
 
-const animationStyle =
-  "before:absolute before:w-1 before:bg-fuchsia-300 before:content-[''] before:transition-all before:duration-300 before:ease-out after:absolute after:h-1 after:bg-fuchsia-300 after:content-[''] after:transition-all after:duration-300 after:ease-out";
+const baseStyle =
+  "relative before:absolute before:w-1 hover:before:bg-fuchsia-300 before:content-[''] before:transition-all before:duration-300 before:ease-out after:absolute after:h-1 hover:after:bg-fuchsia-300 after:content-[''] after:transition-all after:duration-300 after:ease-out before:h-0 after:w-0";
 
-export const buttonActiveStyle = clsx(
-  'before:top-0 before:left-0 before:h-full after:top-0 after:left-0 after:w-full',
-  animationStyle,
+const hoverStyle =
+  'hover:before:bg-fuchsia-300 hover:after:bg-fuchsia-300 hover:before:h-full hover:after:w-full';
+
+export const buttonHoverStyle = clsx(
+  'before:top-0 before:left-0 after:top-0 after:left-0',
+  baseStyle,
+  hoverStyle,
 );
 
-export const buttonInactiveStyle = clsx(
-  'before:top-0 before:right-0 before:h-0 after:top-0 after:right-0 after:w-0 hover:bg-fuchsia-50',
-  animationStyle,
-);
-
-export const divActiveStyle = clsx(
-  'before:bottom-0 before:right-0 before:h-full after:bottom-0 after:right-0 after:w-full',
-  animationStyle,
-);
-
-export const divInactiveStyle = clsx(
-  'before:bottom-0 before:left-0 before:h-0 after:bottom-0 after:left-0 after:w-0 hover:bg-fuchsia-50',
-  animationStyle,
+export const divHoverStyle = clsx(
+  'before:bottom-0 before:right-0 after:bottom-0 after:left-0',
+  baseStyle,
+  hoverStyle,
 );
