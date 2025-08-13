39bc0e7d22e7bd41c00da996cc7f467199575b9b
minji_105 <ajhjmj9@naver.com>
2025-08-13 15:28:01 +0900

    💄 Design: MyPageLayout aside 레이아웃 개선 (#40)

--- src/components/layouts/MyPageLayout.tsx
+++ src/components/layouts/MyPageLayout.tsx
@@ -2,22 +2,23 @@ import { Outlet } from 'react-router-dom';
 
 import Footer from '@/components/common/Footer';
 import { Header } from '@/components/common/header';
-import Nav from '@/components/mypage/Nav';
 import BottomNav from '@/components/mypage/BottomNav';
+import Nav from '@/components/mypage/Nav';
 
 export default function MyPageLayout() {
   return (
     <>
       <Header isLoggedIn={false} />
-      <div className="m-auto mt-16 flex max-w-[1360px] px-6 md:px-10">
-        <aside className="w-55 pr-4">
-          <h2 className="py-10 text-3xl font-semibold">마이페이지</h2>
-
-          <Nav />
+      <div className="m-auto mt-16 flex max-w-[1360px] items-stretch px-6 md:px-10">
+        <aside className="flex w-55 flex-col justify-between pr-4">
+          <div>
+            <h2 className="py-10 text-3xl font-semibold">마이페이지</h2>
+            <Nav />
+          </div>
           <BottomNav />
         </aside>
 
-        <main>
+        <main className="flex-1">
           <Outlet />
         </main>
       </div>
