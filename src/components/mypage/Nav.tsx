f4b4b45edfdcbc3335f45fc8bd88cf646e8e72e0
minji_105 <ajhjmj9@naver.com>
2025-08-13 17:39:20 +0900

    ♻️ Refactor: Nav 컴포넌트 버튼을 네이티브 button으로 변경 (#40)
    - 반응형 스타일 적용을 위해 Button 컴포넌트 사용에서 네이티브 button 사용으로 리팩토링
    - 반응형에 따른 스타일 변경 적용

--- src/components/mypage/Nav.tsx
+++ src/components/mypage/Nav.tsx
@@ -2,14 +2,14 @@ import { ChevronRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { Button } from '@/components/common/Button';
-
 const MYPAGE_NAV = [
   { label: '내 프로필', path: '/mypage/myprofile' },
   { label: '즐겨찾기한 일정', path: '/mypage/myschedule' },
 ];
+const DesktopButtonStyle =
+  'md:inline-flex md:justify-between md:border-none md:w-auto md:rounded-md md:px-4 md:py-4 md:transition-colors md:hover:bg-gray-200';
 
-export default function Nav() {
+export default function Nav({ className }: { className?: string }) {
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -18,22 +18,30 @@ export default function Nav() {
   };
 
   return (
-    <nav className="items-strech flex flex-col pb-10">
+    <nav
+      className={clsx(
+        'items-strech flex w-full md:w-auto md:flex-col md:pb-10',
+        className,
+      )}
+    >
       {MYPAGE_NAV.map(({ label, path }) => {
         const isActive = location.pathname === path;
 
         return (
-          <Button
-            variant="secondary"
+          <button
             className={clsx(
-              'flex w-full !justify-between py-4',
-              isActive ? 'bg-gray-100 font-semibold' : 'bg-white',
+              'w-1/2 border-b-2 py-4',
+              DesktopButtonStyle,
+              isActive
+                ? 'border-gray-700 font-semibold md:bg-gray-100 md:font-semibold'
+                : 'border-gray-200 md:bg-white',
             )}
+            type="button"
             onClick={() => handleClickButton(path)}
           >
             <p>{label}</p>
-            <ChevronRightIcon className="w-5" />
-          </Button>
+            <ChevronRightIcon className="hidden w-5 md:block" />
+          </button>
         );
       })}
     </nav>
