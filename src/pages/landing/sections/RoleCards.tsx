28be6a294293a8f1ba92905dc7e81b0df625c7b8
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-15 02:26:44 +0900

    ✨ Feat: RoleCards 섹션 컴포넌트 추가 (#57)
    헤더 및 카드 컴포넌트 배치 / 스타일 지정

--- src/pages/landing/sections/RoleCards.tsx
+++ src/pages/landing/sections/RoleCards.tsx
@@ -1,7 +1,28 @@
+import Card from '@/components/common/card';
+
 function RoleCards() {
   return (
-    <div>
-      <span>RoleCards</span>
+    <div className="m-auto flex max-w-screen-xl flex-col items-center gap-18 px-4 py-24">
+      <h4 className="text-center text-2xl font-bold">
+        팬·매니저·아이돌, 딩딩이 하나로 연결해 드려요
+      </h4>
+      <div className="flex scale-100 gap-8">
+        <Card
+          type="animation"
+          title="팬"
+          description="공개된 스케줄을 모아 중요 일정만 골라 알림을 받을 수 있어요."
+        />
+        <Card
+          type="animation"
+          title="팬"
+          description="공개된 스케줄을 모아 중요 일정만 골라 알림을 받을 수 있어요."
+        />
+        <Card
+          type="animation"
+          title="팬"
+          description="공개된 스케줄을 모아 중요 일정만 골라 알림을 받을 수 있어요."
+        />
+      </div>
     </div>
   );
 }
