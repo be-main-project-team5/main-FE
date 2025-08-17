99eb27f559b377ae6429986885a3870181830b2c
minji_105 <ajhjmj9@naver.com>
2025-08-17 14:03:58 +0900

    ✨ Feat: 마이페이지 라우팅 설정 (#67)
    마이페이지 공통 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import '@/App.css';
 
 import { Route, Routes } from 'react-router-dom';
 
-import Layout from './components/layouts/Layout';
-import Main from './pages/Main';
+import Layout from '@/components/layouts/Layout';
+import MyPageLayout from '@/components/layouts/MyPageLayout';
+import Main from '@/pages/Main';
+import MyProfile from '@/pages/MyProfile';
+import MySchedule from '@/pages/MySchedule';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Main />} />
+      </Route>
+      <Route path="/mypage" element={<MyPageLayout />}>
+        <Route path="myprofile" element={<MyProfile />} />
+        <Route path="myschedule" element={<MySchedule />} />
       </Route>
     </Routes>
   );
