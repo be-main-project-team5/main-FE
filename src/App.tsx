2bb0b417315b25afa4a04ce75d0b3885fc13c874
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-19 13:47:24 +0900

    ✨ Feat: App.tsx에 /chat 라우트 경로 추가 (#83)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import '@/App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from './components/layouts/Layout';
+import Chat from './pages/chat/Chat';
 import LandingPage from './pages/landing/LandingPage';
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout component="landing" />}>
         <Route index element={<LandingPage />} />
+      </Route>
+      <Route path="/chat" element={<Layout component="chat" />}>
+        <Route index element={<Chat />} />
       </Route>
     </Routes>
   );
