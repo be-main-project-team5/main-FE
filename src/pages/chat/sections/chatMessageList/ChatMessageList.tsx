61b576a1bf5a2fa5ed584d03e7252bcfb17f6cc1
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-21 01:30:34 +0900

    ♻️ Refactor: ChatMessageList 컴포넌트 가상 스크롤 적용 (#86)
    일반적인 채팅방 화면처럼 최초 렌더링 시 맨 아래 데이터부터 보여줌 / 채팅방 화면 아래를 보고 있을 때 새 메시지가 오면 자동으로 스크롤 / 날짜별 단위로 나누어 useMemo로 저장 후 렌더링

--- src/pages/chat/sections/chatMessageList/ChatMessageList.tsx
+++ src/pages/chat/sections/chatMessageList/ChatMessageList.tsx
@@ -1,11 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { Virtuoso } from 'react-virtuoso';
 
 import type { ChatTypes, GroupedChatListTypes } from '../../chat.types';
 import { CHAT_EXAMPLES } from '../../chatSampleData';
 import ChatMessageGroup from './ChatMessageGroup';
 import DateDivider from './DateDivider';
 
+const renderDataByTime = (_, [dKey, dValue]) => (
+  <div>
+    <DateDivider dKey={dKey} />
+
+    {Object.entries(dValue).map(([tKey, tValue]) => (
+      <ChatMessageGroup key={`T-${dKey}-${tKey}`} tKey={tKey} tValue={tValue} />
+    ))}
+  </div>
+);
+
 function ChatMessageList() {
+  const [atBottom, setAtBottom] = useState(true);
   const toKstDate = (iso: string | Date) => {
     const kst = typeof iso === 'string' ? Date.parse(iso) : iso.getTime();
     return new Date(kst + 9 * 60 * 60 * 1000);
@@ -56,26 +68,27 @@ function ChatMessageList() {
     {},
   );
 
+  const dataEntries = useMemo(
+    () => Object.entries(groupedChatData),
+    [groupedChatData],
+  );
+
   useEffect(() => {
     console.log(groupedChatData);
   }, []);
 
   return (
-    <div className="chat-scrollbar flex h-full flex-col overflow-y-auto py-2 pe-2">
-      <div className="mt-auto" />
-      {Object.entries(groupedChatData).map(([dKey, dValue]) => (
-        <div key={`D-${dKey}`}>
-          <DateDivider dKey={dKey} />
-
-          {Object.entries(dValue).map(([tKey, tValue]) => (
-            <ChatMessageGroup
-              key={`T-${dKey}-${tKey}`}
-              tKey={tKey}
-              tValue={tValue}
-            />
-          ))}
-        </div>
-      ))}
+    <div className="h-full">
+      <Virtuoso
+        className="chat-scrollbar flex flex-col py-2 pe-2"
+        data={dataEntries}
+        computeItemKey={(_, [dKey]) => `D-${dKey}`}
+        alignToBottom
+        initialTopMostItemIndex={{ index: 'LAST', align: 'end' }}
+        followOutput={atBottom ? 'smooth' : false}
+        atBottomStateChange={setAtBottom}
+        itemContent={renderDataByTime}
+      />
     </div>
   );
 }
