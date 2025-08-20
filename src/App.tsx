d1cfba1d03442502618542b1cd4412571f5e787d
minji_105 <ajhjmj9@naver.com>
2025-08-20 15:04:54 +0900

    ✨ Feat: 로그인, 회원가입 라우트 설정 (#101)

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,26 @@ import '@/App.css';
 
 import { Route, Routes } from 'react-router-dom';
 
+import AuthLayout from '@/components/layouts/AuthLayout';
 import Layout from '@/components/layouts/Layout';
 import MyPageLayout from '@/components/layouts/MyPageLayout';
 import IdolSearchPage from '@/pages/idolSearch/IdolSearchPage';
 import LandingPage from '@/pages/landing/LandingPage';
+import Login from '@/pages/Login';
 import MyProfile from '@/pages/MyProfile';
 import MySchedule from '@/pages/MySchedule';
+import Register from '@/pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout component="landing" />}>
         <Route index element={<LandingPage />} />
+      </Route>
+
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
       </Route>
 
       <Route path="/search" element={<Layout />}>
