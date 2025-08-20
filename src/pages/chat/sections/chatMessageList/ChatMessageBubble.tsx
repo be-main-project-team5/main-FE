08a46ba4c177c3949c01301eb41469662972c36a
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-20 20:16:27 +0900

    ♻️ Refactor: 수정된 타입 반영 및 map 함수 key 추가 (#86)

--- src/pages/chat/sections/chatMessageList/ChatMessageBubble.tsx
+++ src/pages/chat/sections/chatMessageList/ChatMessageBubble.tsx
@@ -3,10 +3,10 @@ import clsx from 'clsx';
 import { ChatMessageBubbleStyles } from '../../chat.styles';
 import type { ChatMessageBubbleTypes } from '../../chat.types';
 
-function ChatMessageBubble({ isMyChat, content }: ChatMessageBubbleTypes) {
+function ChatMessageBubble({ isMyChat, text }: ChatMessageBubbleTypes) {
   return (
     <div className={clsx(ChatMessageBubbleStyles({ myChat: isMyChat }))}>
-      {content}
+      {text}
     </div>
   );
 }
