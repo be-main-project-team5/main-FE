4b5e2b36dfa8a4306a9f40258a2621a06e15644d
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-20 17:18:06 +0900

    ✨ Feat: ChatMessageGroup 컴포넌트 추가 (#86)
    ChatMessageBubble 컴포넌트를 활용해 상대방 또는 나의 메시지 렌더링

---
+++ src/pages/chat/sections/chatMessageList/ChatMessageGroup.tsx
@@ -0,0 +1,54 @@
+import { cva } from 'class-variance-authority';
+import clsx from 'clsx';
+
+import { UserAvatarImage } from '@/components/common/UserAvatarImage';
+
+import type { GroupedChatTypes } from '../../chat.types';
+import ChatMessageBubble from './ChatMessageBubble';
+
+// *memo - 로그인 시 전역 상태로 저장되는 userId 값이 존재한다고 가정
+const USER_ID = 'idol-01';
+
+const ChatMessageGroupStyles = cva('flex w-full', {
+  variants: {
+    myChat: {
+      true: 'flex-col items-end gap-1',
+      false: ' gap-2',
+    },
+  },
+  defaultVariants: {
+    myChat: false,
+  },
+});
+
+function ChatMessageGroup(data: GroupedChatTypes) {
+  const { sender, contents } = data;
+  const isMyChat = sender.id === USER_ID;
+
+  return (
+    // <div>ChatMessageGroup</div>
+    <div className={clsx(ChatMessageGroupStyles({ myChat: isMyChat }))}>
+      {isMyChat ? (
+        <>
+          {contents.map(content => (
+            <ChatMessageBubble isMyChat={isMyChat} content={content} />
+          ))}
+        </>
+      ) : (
+        <>
+          <UserAvatarImage profileImageUrl={sender.profile_image} />
+          <div className="flex w-full flex-col">
+            <span className="text-xs font-medium text-gray-900">
+              {sender.nickname}
+            </span>
+            {contents.map(content => (
+              <ChatMessageBubble isMyChat={isMyChat} content={content} />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default ChatMessageGroup;
