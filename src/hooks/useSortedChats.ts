21689c1b1f2acd0dd7e7e92883b3832a6a690032
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-21 02:17:04 +0900

    ✨ Feat: 채팅 메시지 데이터 정렬하는 hook 분리 (#86)

---
+++ src/hooks/useSortedChats.ts
@@ -0,0 +1,12 @@
+import { useMemo } from 'react';
+
+import type { ChatTypes } from '@/pages/chat/chat.types';
+
+export const useSortedChats = (rawData: ChatTypes[]) =>
+  useMemo(
+    () =>
+      [...rawData].sort(
+        (a, b) => new Date(a.sendAt).getTime() - new Date(b.sendAt).getTime(),
+      ),
+    [],
+  );
