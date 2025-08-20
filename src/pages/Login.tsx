087aa1c9d08e9643fcb4a89db8498af711203426
minji_105 <ajhjmj9@naver.com>
2025-08-21 02:34:03 +0900

    ✨ Feat: 로그인 폼 UI 추가 (#106)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,62 @@
-export default function Login() {
-  return <>로그인페이지입니다.</>;
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
+import { Link } from 'react-router-dom';
+
+import { Button } from '@/components/common/Button';
+import Input from '@/components/common/input';
+import Select from '@/components/common/Select';
+import { GoogleIcon, KakaoIcon } from '@/components/SocialIcons';
+
+const USER_TYPE = [
+  { id: '일반', label: '일반 회원 (팬)' },
+  { id: '매니저', label: '매니저' },
+  { id: '아이돌', label: '아이돌' },
+];
+
+export default function login() {
+  return (
+    <div className="flex flex-col gap-2">
+      <div>
+        <h1 className="mb-1 text-xl font-bold">로그인</h1>
+        <p>사용자 유형을 선택하고, 이메일과 비밀번호를 입력해주세요.</p>
+      </div>
+
+      <Select className="mt-8" list={USER_TYPE} placeholder="사용자 유형" />
+
+      <Input type="email" label="이메일" />
+      <Input type="password" label="비밀번호" />
+
+      <div className="my-8 flex flex-col gap-4">
+        <Button size="lg" className="w-full font-semibold">
+          로그인
+          <ChevronRightIcon className="ml-2 w-5 md:block" />
+        </Button>
+        <Button
+          variant="white"
+          size="lg"
+          className="w-full border-1 border-gray-300 bg-white font-semibold"
+        >
+          <GoogleIcon className="absolute left-4" />
+          Google로 시작하기
+        </Button>
+        <Button
+          variant="secondary"
+          size="lg"
+          className="bg-kakao-yellow hover:bg-kakao-hover w-full font-semibold"
+        >
+          <KakaoIcon className="absolute left-4" />
+          Kakao로 시작하기
+        </Button>
+      </div>
+
+      <p>
+        DingDing 회원이 아니신가요?
+        <Link
+          to="/auth/register"
+          className="block cursor-pointer font-semibold hover:underline md:ml-2 md:inline"
+        >
+          지금 가입하세요.
+        </Link>
+      </p>
+    </div>
+  );
 }
