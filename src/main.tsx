1c2c25dce38dea8cd697d5212d92f74da2e07682
minji_105 <ajhjmj9@naver.com>
2025-08-21 14:03:17 +0900

    ✨ Feat: 전역에 ToastContainer 추가 (#114)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import App from '@/App';
 
@@ -23,6 +25,11 @@ enableMocking().then(() => {
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <App />
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar={true}
+          />
         </BrowserRouter>
       </QueryClientProvider>
     </StrictMode>,
