38f52cda5b1c9808dc825b13efd331b515108f08
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-23 19:32:47 +0900

    ✅ Test: 배포 환경에서 api 정상 요청 확인 위해 테스트 코드 추가 (#139)

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,9 +1,25 @@
+import { useQueries } from '@tanstack/react-query';
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import {
+  createNewChatRoomAPI,
+  deleteChatRoomAPI,
+  getChatMessageAPI,
+  getChatRoomParticipantsAPI,
+  getMyChatRoomDetailAPI,
+  getMyChatRoomListAPI,
+  joinChatRoomAPI,
+  leaveChatRoomAPI,
+  patchChatRoomDetailAPI,
+  putChatRoomDetailAPI,
+} from '@/api/chatApi';
 
 import ChatComposer from './sections/chatComposer/ChatComposer';
 import ChatContactList from './sections/chatContactList/ChatContactList';
 import ChatMessageList from './sections/chatMessageList/ChatMessageList';
+
+const GROUP_NAME_TEST = '테스트 채팅방';
 
 function Chat() {
   const [isVisible, setIsVisible] = useState(false);
@@ -11,6 +27,60 @@ function Chat() {
   const handleToggleConversationList = () => {
     setIsVisible(prev => !prev);
   };
+
+  // api cors 에러 해결 확인을 위해 일괄 테스트
+  const apiTests = useQueries({
+    queries: [
+      { queryKey: ['getMyChatRoomList'], queryFn: getMyChatRoomListAPI },
+      {
+        queryKey: ['createNewChatRoomA'],
+        queryFn: () => createNewChatRoomAPI(GROUP_NAME_TEST),
+      },
+      {
+        queryKey: ['getMyChatRoomDetail'],
+        queryFn: () => getMyChatRoomDetailAPI(0),
+      },
+      {
+        queryKey: ['putChatRoomDetail'],
+        queryFn: () => putChatRoomDetailAPI(0, '이름 put 수정된 채팅방'),
+      },
+      {
+        queryKey: ['patchChatRoomDetail'],
+        queryFn: () => patchChatRoomDetailAPI(0, '이름 patch 수정된 채팅방'),
+      },
+      {
+        queryKey: ['deleteChatRoom'],
+        queryFn: () => deleteChatRoomAPI(0),
+      },
+      {
+        queryKey: ['joinChatRoom'],
+        queryFn: () => joinChatRoomAPI(0, '채팅방'),
+      },
+      {
+        queryKey: ['leaveChatRoom'],
+        queryFn: () => leaveChatRoomAPI(0, '채팅방'),
+      },
+      {
+        queryKey: ['getChatMessage'],
+        queryFn: () => getChatMessageAPI(0),
+      },
+      {
+        queryKey: ['getChatRoomParticipants'],
+        queryFn: () => getChatRoomParticipantsAPI(0),
+      },
+    ],
+  });
+
+  useEffect(() => {
+    apiTests.forEach((q, i) => {
+      if (q.isSuccess) {
+        console.log(`Query ${i} 성공`, q.data);
+      }
+      if (q.isError) {
+        console.error(`Error ${i} 실패`, q.error);
+      }
+    });
+  }, [apiTests]);
 
   return (
     <div className="relative flex h-[calc(100dvh-64px)]">
