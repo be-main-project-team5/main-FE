abdc0c9483d818d4db975a395acd9de1d071f7a6
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-24 18:06:58 +0900

    ♻️ Refactor: map 함수로 에러나지 않도록 ? 연산자 추가 (#139)

--- src/pages/chat/sections/chatContactList/ChatContactGroup.tsx
+++ src/pages/chat/sections/chatContactList/ChatContactGroup.tsx
@@ -42,7 +42,7 @@ function ChatContactGroup({ isVisible, participants }: ChatContactGroupProps) {
       <div className="flex flex-col gap-5 rounded-2xl bg-gray-50 p-6 shadow-[0_0_15px_3px_#00000015] lg:shadow-none">
         <h4 className="text-lg font-semibold">대화 상대</h4>
         <div className="flex flex-col gap-4">
-          {participants?.results.map((participant: ChatParticipant) => (
+          {participants?.results?.map((participant: ChatParticipant) => (
             <ChatContactItem key={participant.id} contactData={participant} />
           ))}
         </div>
